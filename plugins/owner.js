7487e058cae87d3689df060ed3279d175ac816e1
Shefin-Sir <95840702+Shefin-Sir@users.noreply.github.com>
2021-12-09 11:46:15 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,13 +6,13 @@ if (Config.WORKTYPE == 'private') {
 
     Julie.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '15369524516-1612300121@g.us') {
+        if (message.jid === '120363041544355221@g.us') {
 
             return;
         }
 
         if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*Amalser by Amal*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'*KellyMwol by Shefin*' , MessageType.text);
         }
         else {
             await message.client.sendMessage(message.jid,Config.OWNER + '\n\n---------------------', MessageType.text);
